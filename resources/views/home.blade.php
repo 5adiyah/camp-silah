704e3446420c1a4c9308230e4560fb057b475dff
5adiyah <sadiali@pdx.edu>
2018-10-18 15:05:53 -0700

    Use layout template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,5 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CAMP SILAH</title>
+@extends('layout')
 
-</head>
-<body>
-<h1>hello</h1>
-</body>
-</html>
+@section('content')
+    <h1>hello</h1>
+@endsection
