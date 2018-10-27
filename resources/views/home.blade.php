8c08f0ca0295d1494dd8927242631b944df9554b
5adiyah <sadiali@pdx.edu>
2018-10-27 14:03:15 -0700

    Add theming for quotes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
 @section('content')
-    <h1>Home</h1>
+    <div class="quote-header col12">
+        <div class="quote-mark-l"><img src="{{ asset('images/quote-left.png') }}" ></div>
+        <div class="quote"><p>Camp silah was so much fun! I got to see some new and old friends. I loved every second of the experience!</p></div>
+        <div class="quote-mark-r"><img src="{{ asset('images/quote-right.png') }}" ></div>
+    </div>
 @endsection
