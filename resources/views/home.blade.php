ff95009022bb0aea4ec53c735ee68b7616bce4a8
5adiyah <sadiali@pdx.edu>
2018-10-27 14:30:04 -0700

    Add text box theming

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,4 +6,18 @@
         <div class="quote"><p>Camp silah was so much fun! I got to see some new and old friends. I loved every second of the experience!</p></div>
         <div class="quote-mark-r"><img src="{{ asset('images/quote-right.png') }}" ></div>
     </div>
+
+    <div class="text-box-container col12">
+        <div class="title-box">
+            <h2>WHAT IS CAMP SILAH?</h2>
+        </div>
+
+        <div class="text-box">
+            <p>The word Silah in Arabic means connection, which is the theme of this year's camp. We are hoping that by the end of the camp, each participant will have a better connection with Allah SWT, themselves, and nature. This camp will be for both boys and girls aged 8-18.</p>
+        </div>
+
+        <div class="text-box-cta">
+            <button>Learn More</button>
+        </div>
+    </div>
 @endsection
