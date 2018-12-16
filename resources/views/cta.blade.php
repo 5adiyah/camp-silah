4bf033c27e5ecc8340aafa4471f92a914f659420
5adiyah <sadiali@pdx.edu>
2018-12-16 13:39:39 -0800

    Change cta to its own section

---
+++ resources/views/cta.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('cta')
+    <div class="email-list col12"  style="background-image:url('{{ asset('images/email-pttrn.png') }}')">
+        <h2>Sign up to be the first to know about Camp Silah 2019</h2>
+        <div class="email-cta">
+            <button>Sign Up</button>
+        </div>
+    </div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -42,14 +42,10 @@
         </div>
 
         @yield('content')
+        @yield('cta')
 
-        <div class="email-list col12"  style="background-image:url('{{ asset('images/email-pttrn.png') }}')">
-            <h2>Sign up to be the first to know about Camp Silah 2019</h2>
-            <div class="email-cta">
-                <button>Sign Up</button>
-            </div>
-        </div>
     </div>
+
 
 
     <div class="footer">
